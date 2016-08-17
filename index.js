98e26a4944d9a176e39d8f4a85666278359a3e46
oregami <oregami@163.com>
2016-08-17 07:35:33 +0000

    add token

--- index.js
+++ index.js
@@ -4,23 +4,47 @@ var express = require('express'),
     SessionStore = require('connect-redis')(session),
     log = console.log,
     redis = new require('ioredis')(config.redisUrl),
-    MongoClient = require('mongodb').MongoClient,
     mongoose = require('mongoose'),
     Promise = require('bluebird'),
     Schema = mongoose.Schema,
     bodyParser = require('body-parser'),
+    bcrypt = require('bcryptjs'),
+    bcryptGenSalt  = Promise.promisify(bcrypt.genSalt),
+    bcryptHash     = Promise.promisify(bcrypt.hash),
+    bcryptCompare  = Promise.promisify(bcrypt.compare),
     app = express();
 
-MongoClient.connect("mongodb://localhost:27017/usermicroservice", function (err, mdb) {
-    db = mdb;
-});
 
+function generatePasswordHash(password) {
+    // Generate a new salt
+    return bcryptGenSalt().then(function (salt) {
+        // Hash the provided password with bcrypt
+        return bcryptHash(password, salt);
+    });
+}
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+function uid(len) {
+    var buf = [],
+        chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789',
+        charlen = chars.length,
+        i;
+
+    for (i = 1; i < len; i = i + 1) {
+        buf.push(chars[getRandomInt(0, charlen - 1)]);
+    }
+
+    return buf.join('');
+}
 
 var userSchema = new Schema({
     id:  String,
     password: String,
     type:   String,
-    create_date: { type: Date, default: Date.now },
+    create_date: { type: Date, default: new Date().toLocaleString() },
 });
 
 var User = mongoose.model('User', userSchema);
@@ -44,23 +68,76 @@ app.use(session({
 }));
 
 app.get('/', function (req, res) {
-    log(req.session);
     res.send(req.session);
 });
 
 app.get('/setup/', function (req, res) {
-    console.log(req.query.userID, req.query.password);
     var user = new User({ id: req.query.userID, password: req.query.password});
     User.findOne({id: req.query.userID}).exec().then(function (data) {
         if (data) {
             res.send('already exist');
         } else {
-            user.save().then(function (data) {
-                res.send(data);
+            generatePasswordHash(user.password).then(function(hash) {
+                user.password = hash;
+                user.save().then(function (data) {
+                    res.send(data);
+            });
             });
         }
     });
 
+});
+
+app.get('/login/', function(req, res) {
+    User.findOne({id: req.query.userID}).exec().then(function (data) {
+        if (data) {
+            bcryptCompare(req.query.password, data.password).then(function(matched) {
+                if (matched) {
+                    data = data.toObject();
+                    delete data.password;
+                    req.session.user = data;
+                    req.session.save();
+                    var token = uid(256);
+                    redis.set('token:'+ token, req.session.user.id, 'EX',30 * 24 * 3600)
+                        .then(function () {
+                            res.send(token);
+                        })
+                    }
+            });
+        } else {
+            res.send('no user');
+        }
+    });
+});
+
+app.get('/token/', function(req, res){
+    if (req.session && req.session.user) {
+        var token = uid(256);
+        redis.set('token:'+ token, req.session.user.id, 'EX',30 * 24 * 3600)
+            .then(function () {
+                res.send(token);
+            })
+    }
+});
+
+app.get('/user/', function(req, res) {
+   if (req.session && req.session.user) {
+       res.send(req.session.user);
+   } else if (req.query.userToken){
+       redis.get('token:'+ req.query.userToken).then(function (userID) {
+          if (userID) {
+              User.findOne({id: userID}).exec().then(function (data) {
+                  data = data.toObject();
+                  delete data.password;
+                  res.send(data);
+              });
+          } else {
+              res.send('need login');
+          }
+       });
+   } else {
+       res.send('need login');
+   }
 });
 
 app.listen(80);
